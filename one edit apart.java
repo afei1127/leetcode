20daef2cfde7168f00a5a93bb996c21b79620bc9
afei1127 <34657209+afei1127@users.noreply.github.com>
2018-07-10 15:28:35 -0700

    Create one edit apart.java

---
+++ one edit apart.java
@@ -0,0 +1,42 @@
+/**
+ * 161. One Edit Distance O(n) O(1)?
+ *
+ * Given two strings s and t, determine if they are both one edit distance apart.
+ *
+ * Note:
+ *
+ * There are 3 possiblities to satisify one edit distance apart:
+ *
+ *     Insert a character into s to get t
+ *     Delete a character from s to get t
+ *     Replace a character of s to get t
+ *
+ * Example 1:
+ * Input: s = "ab", t = "acb"
+ * Output: true
+ * Explanation: We can insert 'c' into s to get t.
+ *
+ * Example 2:
+ * Input: s = "cab", t = "ad"
+ * Output: false
+ * Explanation: We cannot get t from s by only one step.
+ *
+ * Example 3:
+ * Input: s = "1203", t = "1213"
+ * Output: true
+ * Explanation: We can replace '0' with '1' to get t.
+ */
+public boolean isOneEditDistance(String s, String t) {
+    for (int i = 0; i < Math.min(s.length(), t.length()); i++) { 
+    	if (s.charAt(i) != t.charAt(i)) {
+    		if (s.length() == t.length()) // s has the same length as t, so the only possibility is replacing one char in s and t
+    			return s.substring(i + 1).equals(t.substring(i + 1));
+			else if (s.length() < t.length()) // t is longer than s, so the only possibility is deleting one char from t
+				return s.substring(i).equals(t.substring(i + 1));	        	
+			else // s is longer than t, so the only possibility is deleting one char from s
+				return t.substring(i).equals(s.substring(i + 1));
+    	}
+    }       
+    //All previous chars are the same, the only possibility is deleting the end char in the longer one of s and t 
+    return Math.abs(s.length() - t.length()) == 1;        
+}
